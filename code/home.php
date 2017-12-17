cbd667eb34b47ca118626bac5d2748ecbb5de598
Muhammad Hamza Khan <muhammadhamzakhan008@gmail.com>
2017-12-17 13:10:35 -0800

    bootstrap added to home

--- code/home.php
+++ code/home.php
@@ -1,5 +1,5 @@
 <?php
-include 'connect.php';
+include 'bootstrap/connect.php';
 $userID = $_SESSION['userid'];
 $category = "";
 $connected = FALSE;
@@ -26,29 +26,84 @@ function test_input($data) {
 }
 ?>
 
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Servo Home Page </title>
-<link href="css/homepage.css" rel="stylesheet" type="text/css">
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
 
-<body>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+  </head>
+  <body style="padding-top: 70px">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.3.min.js"></script>
 
-	<div id="navbar">
-		<ul>
-			<li><a href="homepage.html" title="Home Page Link">Home</a></li>
-			<li><a href="messages.html">Messages</a></li>
-			<li><a href="#">Notifications</a></li>
-			<li><a href="settings.html">Settings</a></li>
-			<li><a href="logout.php">Logout</a></li>    
-		</ul>
-	</div>
-	<br>
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
 	
-	<div id="topicbrowser">
-		<h2>Browse Existing Topics:</h2>
+  <div class="container-fluid">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+          <a class="navbar-brand" href="#">Servo</a></div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="topFixedNavbar1">
+          <ul class="nav navbar-nav">
+            <li class="active"></li>
+            <li></li>
+            <li class="dropdown">
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="bootstrap/homepage.html" title="Home Page Link">Home</a></li>
+            <li><a href="bootstrap/messages.html">Messages</a></li>
+            <li><a href="#">Notifications</a></li>
+            <li><a href="bootstrap/settings.html">Settings</a></li>
+            <li><a href="bootstrap/logout.php">Logout</a></li>
+            <li class="dropdown">
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+    <div class="row">
+      <div class="col-sm-4" id="topicbrowser">
+		  <h3>Browse Existing Topics:</h3>
 			<?php		
 		
 			$db = $conn;
@@ -88,14 +143,14 @@ function test_input($data) {
 				}
 			}
 			?>	
-	</div>
-
-<div id = middlecolumn>
+		</div>
+		
+      <div class="col-sm-4" id = "middlecolumn">
 		<div id="postnewtopic">	
 			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 
-			    <h2>Create A New Topic:</h2>
-                  <textarea name="comment" rows="2"  cols="70"><?php echo $comment;?></textarea>
+			    <h3>Create A New Topic:</h3>
+                  <textarea name="comment" rows="2"><?php echo $comment;?></textarea>
 				<br><br>
 				<?php 
 				$sql = "SELECT name from Category where 1";
@@ -116,7 +171,7 @@ function test_input($data) {
 		</div>
 		
 	<div id="friendsactivity">
-		<h2>Your Friends' Activity Is As Following: </h2>
+		<h3>Your Friends' Activity Is As Following: </h3>
 		<ul>
 				<?php
 				$db = $conn;
@@ -162,10 +217,10 @@ function test_input($data) {
 				?>
 		</ul>
 		</div>
-	</div>
-	
-<div id="usertopicsbar">
-	<h2>Your Topics:</h2>
+		</div>
+      <div class="col-sm-4" id="rightbar">
+		<div id="usertopicsbar">
+	<h3>Your Topics:</h3>
 		<ul>
 			<?php 
 
@@ -187,7 +242,7 @@ function test_input($data) {
 	</div>	
 	
 	<div id="favtopicsbar">
-		<h2>Your Favorite Topics:</h2>
+		<h3>Your Favorite Topics:</h3>
 		<ul>	
 			<?php
 
@@ -206,7 +261,6 @@ function test_input($data) {
 			?>
 		</ul>
 	</div>
-
-
-</body>
+  </div>
+  </body>
 </html>
