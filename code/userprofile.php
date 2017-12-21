0ff4a0dbef58e515b589724520b9e1b948372fa0
Muhammad Hamza Khan <muhammadhamzakhan008@gmail.com>
2017-12-21 15:52:14 -0800

    created userprofile.php

--- code/userprofile.php
+++ code/userprofile.php
@@ -1,10 +1,73 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+	include 'connect.php';
+	$userID = $_SESSION['userid'];
 
-<body>
-</body>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	  
+  </head>
+  <body style="padding-top: 70px">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.3.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
+	
+  <div class="container-fluid">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+          <a class="navbar-brand" href="home.php">Servo</a></div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="topFixedNavbar1">
+          <form class="navbar-form navbar-left" role="search" method="post">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search" name="searchInput">
+            </div>
+            <button type="submit" class="btn btn-default" name="searchButton">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="home.php" title="Home Page Link">Home</a></li>
+            <li><a href="messages.php">Messages</a></li>
+            <li><a href="settingsadmin.php">Settings</a></li>
+            <li><a href="logout.php">Logout</a></li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+    
+    <div class="row">
+  		<div class="col-sm-5" id="leftcolumn">		  
+    		
+		</div>		
+		
+	  	<div class="col-sm-5" id = "middlecolumn">
+		
+		</div>
+     	
+      	<div class="col-sm-5" id="rightcolumn">
+      
+		</div>
+	</div>
+  </body>
 </html>
