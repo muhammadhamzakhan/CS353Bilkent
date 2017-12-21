b3e863133373574ec4f8ae9a33fef7f8de32aa3a
Muhammad Hamza Khan <muhammadhamzakhan008@gmail.com>
2017-12-21 15:23:46 -0800

    removed notifications menu

--- code/settingsadmin.php
+++ code/settingsadmin.php
@@ -137,42 +137,17 @@
           <a class="navbar-brand" href="home.php">Servo</a></div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="topFixedNavbar1">
-          <ul class="nav navbar-nav">
-            <li class="active"></li>
-            <li></li>
-            <li class="dropdown">
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form class="navbar-form navbar-left" role="search">
+          <form class="navbar-form navbar-left" role="search" method="post">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
+              <input type="text" class="form-control" placeholder="Search" name="searchInput">
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-default" name="searchButton">Submit</button>
           </form>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="home.php" title="Home Page Link">Home</a></li>
-            <li><a href="#">Messages</a></li>
-            <li><a href="#">Notifications</a></li>
+            <li><a href="messages.php">Messages</a></li>
             <li><a href="settingsadmin.php">Settings</a></li>
             <li><a href="logout.php">Logout</a></li>
-            <li class="dropdown">
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-              </ul>
-            </li>
           </ul>
         </div>
         <!-- /.navbar-collapse -->
