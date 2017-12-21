387475c2db1a174fa1a4e479b2db0b1b0525413a
Muhammad Hamza Khan <muhammadhamzakhan008@gmail.com>
2017-12-21 14:14:57 -0800

    updated signup.php

--- code/signup.php
+++ code/signup.php
@@ -74,20 +74,58 @@
 <!doctype html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<title>Untitled Document</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Untitled Document</title>
+	<link href="../css/bootstrap.css" rel="stylesheet" type="text/css">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 	</head>
 
 	<body>
 		<h1>We Are Glad That You Decided To Join Our Community! </h1>
-		<form method="POST">
-		  	<label for="username">Username:</label>
-		  	<input type="text" name="username" id="username">
-		  	<label for="email">Email:</label>
-		 	<input type="email" name="email" id="email">
-		  	<label for="password">Password:</label>
-		  	<input type="password" name="password" id="password">
-		  	<input type="submit" name="submit" id="submit" value="Lets Go!">
-		</form>
+		<div class="container-fluid" align="center">
+			<form method="POST">
+				<div class="row" style="margin-top:20px;">
+					<div class="form-group">	   
+						<div class="col-sm-3 col-sm-offset-1">
+							<label for="username" >Username:</label>
+							<input type="text" name="username" id="username">
+						</div>
+					</div>
+				</div>
+
+				<div class="row" style="margin-top:20px;">
+					<div class="form-group">	   
+						<div class="col-sm-3 col-sm-offset-1">
+							<label for="email">Email:</label>
+							<input type="email" name="email" id="email">
+						</div>
+					</div>
+				</div>
+
+				<div class="row" style="margin-top:20px;">
+					<div class="form-group">	   
+						<div class="col-sm-3 col-sm-offset-1">
+							<label for="password">Password:</label>
+							<input type="password" name="password" id="password">
+						</div>
+					</div>
+				</div>
+
+				<div class="row" style="margin-top:20px;">
+					<div class="form-group">	   
+						<div class="col-sm-3 col-sm-offset-1">
+							<input type="submit" name="submit" id="submit" value="Lets Go!">
+						</div>
+					</div>
+				</div>
+      		</form>							
+		</div>
 	</body>
 </html>
